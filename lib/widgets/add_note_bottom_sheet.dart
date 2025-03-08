f2bd3dfe1006fa4bca03c454827a10f4543b9f79
Hossam Ahmed <ha954074@gmail.com>
2025-03-08 20:34:17 +0200

    create cubit to add note

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
+import 'add_note_form.dart';
 import 'custom_button.dart';
 import 'custom_text_field.dart';
 
@@ -18,48 +19,6 @@ class AddNoteBottonSheet extends StatelessWidget {
   }
 }
 
-class AddNotesForm extends StatefulWidget {
-  const AddNotesForm({
-    super.key,
-  });
 
-  @override
-  State<AddNotesForm> createState() => _AddNotesFormState();
-}
-
-class _AddNotesFormState extends State<AddNotesForm> {
-
-  final GlobalKey<FormState> globalKey =GlobalKey();
-  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
-  String? title , content;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: globalKey,
-      autovalidateMode: autoValidateMode,
-      child: Column(
-        children:  [
-          SizedBox(height: 32),
-          CustomTextField(hint: 'Title',onSaved: (val)=> title = val,),
-          SizedBox(height: 20),
-          CustomTextField(hint: 'Content', maxLine: 5,onSaved: (val) => content = val ,),
-          SizedBox(height: 24,),
-          CustomButton(text: 'Add',onTap: () {
-            if(globalKey.currentState!.validate()){
-              globalKey.currentState!.save();
-            }else{
-
-              autoValidateMode =AutovalidateMode.always;
-              setState(() {
-
-              });
-            }
-          },),
-          SizedBox(height: 32,),
-        ],
-      ),
-    );
-  }
-}
 
 
---
+++ lib/widgets/add_note_form.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+
+import 'custom_button.dart';
+import 'custom_text_field.dart';
+
+class AddNotesForm extends StatefulWidget {
+  const AddNotesForm({
+    super.key,
+  });
+
+  @override
+  State<AddNotesForm> createState() => _AddNotesFormState();
+}
+
+class _AddNotesFormState extends State<AddNotesForm> {
+
+  final GlobalKey<FormState> globalKey =GlobalKey();
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+  String? title , content;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: globalKey,
+      autovalidateMode: autoValidateMode,
+      child: Column(
+        children:  [
+          SizedBox(height: 32),
+          CustomTextField(hint: 'Title',onSaved: (val)=> title = val,),
+          SizedBox(height: 20),
+          CustomTextField(hint: 'Content', maxLine: 5,onSaved: (val) => content = val ,),
+          SizedBox(height: 24,),
+          CustomButton(text: 'Add',onTap: () {
+            if(globalKey.currentState!.validate()){
+              globalKey.currentState!.save();
+            }else{
+
+              autoValidateMode =AutovalidateMode.always;
+              setState(() {
+
+              });
+            }
+          },),
+          SizedBox(height: 32,),
+        ],
+      ),
+    );
+  }
+}
